b5c88c4fac1620cef6d4f3eddfa1b5c8fd8bc729
Charly OG <caog.freeman@gmail.com>
2018-05-07 22:43:40 -0500

    Agregadas dos pantallas, en proceso los resultados de busqueda

--- app/src/main/java/palrestaurant/emm/pal_restaurant/Registro.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/Registro.java
@@ -9,17 +9,28 @@ import android.widget.Button;
 public class Registro extends AppCompatActivity {
 
     Button btnCom;
+    Button btnRest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         btnCom = findViewById(R.id.btnCom);  //Creamos el botón que nos llevará a registrar comensal
+        btnRest = findViewById(R.id.btnRest); //Enlazamos con el botón que nos llevará a registrar al restaurante
 
         btnCom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentReg = new Intent(Registro.this, Registrar_Comensal.class); //"Llamamos" al registro de comensal desde pantalla actual
+                Registro.this.startActivity(intentReg);  //Comenzar la actividad de registrar comensal
+
+            }
+        });
+
+        btnRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentReg = new Intent(Registro.this, activity_registrar_restaurante.class); //"Llamamos" al registro de comensal desde pantalla actual
                 Registro.this.startActivity(intentReg);  //Comenzar la actividad de registrar comensal
 
             }
