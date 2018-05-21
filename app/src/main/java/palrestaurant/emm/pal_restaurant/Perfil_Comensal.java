ce9bb383b9490ec58fe8c4bd12ca09d7d91ca5dd
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2018-05-20 22:52:15 -0500

    Agregue Perfil de comensal y funciones no verificadas de registrar comensal

---
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/Perfil_Comensal.java
@@ -0,0 +1,13 @@
+package palrestaurant.emm.pal_restaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil_Comensal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil__comensal);
+    }
+}
--- app/src/main/java/palrestaurant/emm/pal_restaurant/Registrar_Comensal.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/Registrar_Comensal.java
@@ -7,70 +7,99 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Registrar_Comensal extends AppCompatActivity implements View.OnClickListener {
+import java.util.HashMap;
 
-    EditText etNombre, etNombre_Usuario, etContraseña;
-    Button btnCrear;
+public class Registrar_Comensal extends AppCompatActivity {
+
+    private static final String IP_REGISTRAR = "hhttp://pruebagamash.esy.es/archPHP/Registrar_Comensal_INSERT.php";
+
+    EditText nombre, nombre_usuario, pass;
+    Button registrar;
+    private VolleyRP volley;
+    private RequestQueue mRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar__comensal);
 
-        etNombre=(EditText)findViewById(R.id.EditT_Nombre);
-        etNombre_Usuario=(EditText)findViewById(R.id.EditT_Nombre_Usuario);
-        etContraseña=(EditText)findViewById(R.id.EditT_Contraseña);
-        btnCrear=(Button)findViewById(R.id.btnCrear);
+        volley = VolleyRP.getInstance(this);
+        mRequest = volley.getRequestQueue();
 
-        btnCrear.setOnClickListener(new View.OnClickListener() {
+        nombre = (EditText) findViewById(R.id.EditT_Nombre);
+        nombre_usuario=  (EditText) findViewById(R.id.EditT_Nombre_Usuario);
+        pass = (EditText) findViewById(R.id.EditT_Contraseña);
+        registrar = (Button) findViewById(R.id.btnCrear);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
+                registrarWebService(nombre.getText().toString(),nombre_usuario.getText().toString(),pass.getText().toString());
             }
         });
     }
 
-    @Override
-    public void onClick(View view) {
-        final String Nombre_Usuario=etNombre_Usuario.getText().toString();
-        final String Contraseña=etContraseña.getText().toString();
-        final String Nombre=etNombre.getText().toString();
+    private void registrarWebService(String nombre, String nombre_usuario, String pass){
+        HashMap<String,String> hashMapToken = new HashMap<>();
+        hashMapToken.put("Nombre", nombre);
+        hashMapToken.put("Nombre_Usuario", nombre_usuario);
+        hashMapToken.put("Contraseña", pass);
 
-        Response.Listener<String> respoListener = new Response.Listener<String>() {
+        JsonObjectRequest solicitud = new JsonObjectRequest(Request.Method.POST, IP_REGISTRAR, new JSONObject(hashMapToken), new Response.Listener<JSONObject>(){
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONObject datos) {
                 try {
-                    JSONObject jsonResponse = new JSONObject(response);
-                    boolean Exito = jsonResponse.getBoolean("Exito");
-
-                    if(Exito){
-                        Intent intent = new Intent(Registrar_Comensal.this, MainActivity.class);
-                        Registrar_Comensal.this.startActivity(intent);
-                    }
-                    else
-                    {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Registrar_Comensal.this);
-                        builder.setMessage("Error en registro")
-                                .setNegativeButton("Reintentar", null)
-                                .create().show();
+                    String estado = datos.getString("resultado");
+                    if (estado.equalsIgnoreCase("El usuario se registró correctamente")) {
+                        Toast.makeText(Registrar_Comensal.this, estado, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(Registrar_Comensal.this, estado, Toast.LENGTH_SHORT).show();
                     }
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    Toast.makeText(Registrar_Comensal.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
                 }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Registrar_Comensal.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        VolleyRP.addToQueue(solicitud, mRequest, this, volley);
+    }
 
+    /*private void SubirToken(String id,String token){
+        SolicitudesJson s = new SolicitudesJson() {
+            @Override
+            public void solicitudCompletada(JSONObject j) {
+                Toast.makeText(Registro.this, "Se registro correctamente", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            @Override
+            public void solicitudErronea() {
+                Toast.makeText(Registro.this, "No se pudo subir el token", Toast.LENGTH_SHORT).show();
+                finish();
             }
         };
 
-        RegisterRequest registerRequest = new RegisterRequest(Nombre_Usuario,Contraseña,Nombre,respoListener);
-        RequestQueue queue = Volley.newRequestQueue(Registrar_Comensal.this);
-        queue.add(registerRequest);
-    }
+        HashMap<String,String> hashMapToken = new HashMap<>();
+        hashMapToken.put("id",id);
+        hashMapToken.put("token",token);
+
+        s.solicitudJsonPOST(this,SolicitudesJson.IP_TOKEN_UPLOAD,hashMapToken);
+    }*/
 }
