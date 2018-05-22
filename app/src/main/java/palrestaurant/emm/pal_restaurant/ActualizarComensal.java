886724be650f1552b60b67eb8c06649c143e28ca
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2018-05-21 22:57:11 -0500

    Actualizar Comensal y Buscar Platillo, solo pantallas.

---
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/ActualizarComensal.java
@@ -0,0 +1,13 @@
+package palrestaurant.emm.pal_restaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActualizarComensal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_comensal);
+    }
+}
--- app/src/main/java/palrestaurant/emm/pal_restaurant/Buscar_Platillo.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/Buscar_Platillo.java
@@ -2,6 +2,8 @@ package palrestaurant.emm.pal_restaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Buscar_Platillo extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Buscar_Platillo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar__platillo);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> Adaptador = new ArrayAdapter<String>(Buscar_Platillo.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.precios));
+                Adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mySpinner.setAdapter(Adaptador);
     }
 }
