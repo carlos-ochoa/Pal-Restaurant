bf1e500f5f18d0d482dc4bbf144506b96831b4c9
Joaquín Domínguez Moran <joaks.jdm@gmail.com>
2018-05-22 18:54:56 -0500

    Agregue funcionalidad

--- app/src/main/java/palrestaurant/emm/pal_restaurant/activity_responder_encuesta.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/activity_responder_encuesta.java
@@ -8,13 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class activity_responder_encuesta extends AppCompatActivity {
 
+    private static final String IP_REGISTRAR = "http://pruebagamash.esy.es/archPHP/insertar_encuesta_post.php";
     Button siguiente;
     RadioButton r1,r2,r3,r4,r5;
     TextView pregunta;
-    int i=0, resp=0;
+    int i=0;
     String [ ] preguntas= { "La rapidez con la que le fue otorgado el servicio",
                             "¿Cómo calificaría la limpieza del restaurante?",
                             "La calidad del servicio recibido ha sido",
@@ -24,12 +37,15 @@ public class activity_responder_encuesta extends AppCompatActivity {
                             "¿Cumplieron con lo que solicitó?",
                             "¿Cuáles son las probabilidades de que recomiende el restaurante?"};
     int respuestas []= new int[8];
-
+    private VolleyRP volley;
+    private RequestQueue mRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_responder_encuesta);
+        volley = VolleyRP.getInstance(this);
+        mRequest = volley.getRequestQueue();
         siguiente = findViewById(R.id.btnSiguiente);
         pregunta = findViewById(R.id.text);
         r1=findViewById(R.id.radioButton1);
@@ -42,7 +58,7 @@ public class activity_responder_encuesta extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 i++;
-                if(i!=7)
+                if(i<7)
                 {
                     if (r1.isChecked())
                         respuestas[i]=5;
@@ -57,18 +73,56 @@ public class activity_responder_encuesta extends AppCompatActivity {
                     pregunta.setText(preguntas[i]);
 
                 }
-                if(i==7)
-                {
+                if(i==7) {
                     siguiente.setText("Finzalizar encuesta");
-
+                    insertar_respuestas(123, respuestas[0], respuestas[1], respuestas[2], respuestas[3], respuestas[4],
+                            respuestas[5], respuestas[6], respuestas[7]);
                 }
-
-
             }
         });
 
 
     }
+
+    private void insertar_respuestas(
+            int idRestaurante,int Pre1,int Pre2,int Pre3, int Pre4,int Pre5,
+            int Pre6, int Pre7, int Pre8
+    ){
+        HashMap<String,Integer> hashMapToken = new HashMap<>();
+        hashMapToken.put("ID_Restaurante", idRestaurante);
+        hashMapToken.put("Valor_Pre1", Pre1);
+        hashMapToken.put("Valor_Pre2", Pre2);
+        hashMapToken.put("Valor_Pre3", Pre3);
+        hashMapToken.put("Valor_Pre4", Pre4);
+        hashMapToken.put("Valor_Pre5", Pre5);
+        hashMapToken.put("Valor_Pre6", Pre6);
+        hashMapToken.put("Valor_Pre7", Pre7);
+        hashMapToken.put("Valor_Pre8", Pre8);
+
+        JsonObjectRequest solicitu = new JsonObjectRequest(Request.Method.POST, IP_REGISTRAR, new JSONObject(hashMapToken), new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject datos) {
+                try {
+                    String estado = datos.getString("Resultado");
+                    if (estado.equalsIgnoreCase("Gracias por contestar")) {
+                        Toast.makeText(activity_responder_encuesta.this, estado, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(activity_responder_encuesta.this, estado, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(activity_responder_encuesta.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity_responder_encuesta.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+        VolleyRP.addToQueue(solicitu, mRequest, this, volley);
+    }
+
 
 
 }
