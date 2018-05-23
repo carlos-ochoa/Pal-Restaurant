d48ee866500ee5e16b3ab1e25ac65ba3523bcd05
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2018-05-22 23:55:16 -0500

    Avance en eliminar cuentas

--- app/src/main/java/palrestaurant/emm/pal_restaurant/Perfil_Comensal.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/Perfil_Comensal.java
@@ -5,19 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class Perfil_Comensal extends AppCompatActivity {
 
+    private static final String IP_BORRCOM = "http://pruebagamash.esy.es/archPHP/Borrar_Comensal.php";
+
     Button btnBuscar, btnCambiar, btnEliminar;
+    TextView TVNombre_Usuario, TVNombre;
+    private VolleyRP volley;
+    private RequestQueue mRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil__comensal);
 
+        volley = VolleyRP.getInstance(this);
+        mRequest = volley.getRequestQueue();
         btnBuscar = findViewById(R.id.btnBuscar);
         btnCambiar = findViewById(R.id.btnCambiar);
         btnEliminar = findViewById(R.id.btnEliminar);
+        TVNombre = findViewById(R.id.TVNombre);
+        TVNombre_Usuario = findViewById(R.id.TVNombre_Usuario);
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,9 +61,40 @@ public class Perfil_Comensal extends AppCompatActivity {
         btnEliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                BorrarWebService(TVNombre.getText().toString(),TVNombre_Usuario.getText().toString());
+            }
 
+            private void BorrarWebService(String TVNombre, String TVNombre_Usuario) {
+                HashMap<String,String> hashMapToken = new HashMap<>();
+                hashMapToken.put("Nombre_Usuario", TVNombre_Usuario);
+                hashMapToken.put("Nombre", TVNombre);
+
+                JsonObjectRequest solicitud = new JsonObjectRequest(Request.Method.POST, IP_BORRCOM, new JSONObject(hashMapToken), new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject datos) {
+                        try {
+                            String estado = datos.getString("resultado");
+                            if (estado.equalsIgnoreCase("El usuario se borró correctamente")) {
+                                Toast.makeText(Perfil_Comensal.this, estado, Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(Perfil_Comensal.this, estado, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(Perfil_Comensal.this, "No se pudo borrar al usuario", Toast.LENGTH_SHORT).show();
+                        }
                     }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Perfil_Comensal.this, "No se pudo borrar al usuario", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                VolleyRP.addToQueue(solicitud, mRequest, Perfil_Comensal.this, volley);
+
+            }
         });
 
     }
+
 }
--- app/src/main/java/palrestaurant/emm/pal_restaurant/perfilRestaurante.java
+++ app/src/main/java/palrestaurant/emm/pal_restaurant/perfilRestaurante.java
@@ -8,13 +8,38 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.awt.Button;
+import java.util.HashMap;
 
 public class perfilRestaurante extends AppCompatActivity {
 
+    private static final String IP_BORRCOM = "http://pruebagamash.esy.es/archPHP/Borrar_Comensal.php";
+
+    View btnEliminar;
+    private VolleyRP volley;
+    private RequestQueue mRequest;
+    TextView TVNombreRest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_restaurante);
+
+        btnEliminar = findViewById(R.id.btnEliminar);
+        TVNombreRest = findViewById(R.id.TVNombreRest);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,6 +57,49 @@ public class perfilRestaurante extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        volley = VolleyRP.getInstance(this);
+        mRequest = volley.getRequestQueue();
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BorrarWebService(TVNombreRest.getText().toString());
+
+            }
+        });
+
+
+
+    }
+
+    private void BorrarWebService(String TVNombreRest) {
+        HashMap<String,String> hashMapToken = new HashMap<>();
+        hashMapToken.put("Nombre", TVNombreRest);
+
+        JsonObjectRequest solicitud = new JsonObjectRequest(Request.Method.POST, IP_BORRCOM, new JSONObject(hashMapToken), new Response.Listener<JSONObject>(){
+            @Override
+            public void onResponse(JSONObject datos) {
+                try {
+                    String estado = datos.getString("resultado");
+                    if (estado.equalsIgnoreCase("El usuario se borró correctamente")) {
+                        Toast.makeText(perfilRestaurante.this, estado, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(perfilRestaurante.this, estado, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(perfilRestaurante.this, "No se pudo borrar al usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(perfilRestaurante.this, "No se pudo borrar al usuario", Toast.LENGTH_SHORT).show();
+            }
+        });
+        VolleyRP.addToQueue(solicitud, mRequest, perfilRestaurante.this, volley);
+
     }
 
 }
